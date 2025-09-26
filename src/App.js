bfc792bef3d5514738a9e764c003ecbdd4194641
mateusvieira653 <vieira.silva.mateus@escola.pr.gov.br>
2025-09-26 09:30:01 -0300

    componente commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/NavBar";
+import Home from "./components/Home";
+import Escola from "./components/Escola";
+import SobreMim from "./components/SobreMim";
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export  default function App(){
+return (
+  <div className="font-sans">
+    <Navbar />
+    <Home />
+    <Escola />
+    <SobreMim />
+  </div>
+);
 }
-
-export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from "react ";
+ export default function Home (){
+    return (
+        <section id="home" className="p-10 text-center">
+            <h1 className="text-3xl font-bold">Ben-vindo á página inicial</h1>
+            <p className="mt-4 text-gray-700">Esta é a seção Home.</p>
+        </section>
+    );
+ }
